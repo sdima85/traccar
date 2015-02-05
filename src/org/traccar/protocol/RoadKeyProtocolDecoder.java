2f2149415ea1d405d5abe79a20083d353bdbed3b
Дмитрий Слободянюк <kolos.dima@gmail.com>
2015-02-05 17:43:29 +0200

    RoadKey - доработка

--- src/org/traccar/protocol/RoadKeyProtocolDecoder.java
+++ src/org/traccar/protocol/RoadKeyProtocolDecoder.java
@@ -31,6 +31,9 @@ import org.traccar.model.Device;
 import org.traccar.model.ExtendedInfoFormatter;
 import org.traccar.model.Position;
 
+import java.util.Base64;
+import org.traccar.helper.ChannelBufferTools;
+
 public class RoadKeyProtocolDecoder extends BaseProtocolDecoder {
 
     private Long deviceId;
@@ -51,8 +54,26 @@ public class RoadKeyProtocolDecoder extends BaseProtocolDecoder {
             "([NS])," +
             "(\\d{3})(\\d{2}\\.\\d+)," +   // Longitude
             "([EW])," +
+            "(\\d{0,}),"+//тип решение
+            "(\\d{0,}),"+//количество используемых спутников
+            "([\\d\\.]{0,}),"+//геометрический фактор, HDOP
+            "(\\d{0,}\\.\\d+),"+//высота над уровнем моря в метрах
             ".+");
 
+    Pattern patternPRKA = Pattern.compile(
+            "\\$PRKA," +
+            "(\\w{0,2}),"+ //Младший байт идентификатора путевой точки. HEX
+            //"(\\d{0,2})(\\d{0,2})(\\d{0,2}),"+ //UTC дата по часам НТ. - бывает пустым
+            "(\\d{0,2})(\\d{0,2})(\\d{0,2}),"+ //UTC дата по часам НТ.
+            "(\\d{2})(\\d{2})(\\d{2})\\.?\\d*," + // Time                    
+            "(\\w{0,}),"+//Событие
+            "(\\w{0,})\\*"+//Состояние 
+            ".+");
+    
+    Pattern patternPRKB = Pattern.compile("\\$PRKB,(\\d{0,}),([\\w=/+]{0,})\\*\\w{2}");
+    
+    Pattern patternPRKZ = Pattern.compile("\\$PRKZ,([\\w=/+]{0,})\\*\\w{2}");
+    
     private void identify(String id) {
         try {
             Device device = getDataManager().getDeviceByImei(id);
@@ -85,6 +106,11 @@ public class RoadKeyProtocolDecoder extends BaseProtocolDecoder {
             identify(sentence.substring(1, 15));            
             if (channel != null) {
                 channel.write("lt\r\n");
+                //channel.write("puts {$+}; puts {>> set BALANCE}; puts \"## [set BALANCE]\"; puts {$-}; lt");
+                //channel.write(new byte[]{2,3});
+                //channel.write("set BALANCE\r\n");
+                //channel.write("puts $BALANCE\r\n");
+                //channel.write("cfg get\r\n");
             }
             return null;
         }      
@@ -96,25 +122,90 @@ public class RoadKeyProtocolDecoder extends BaseProtocolDecoder {
         }
         //Старт - 
         if (sentence.startsWith("$PRKA")) {
+            
+            Matcher parser = patternPRKA.matcher(sentence);
+            if (!parser.matches()) {
+                return null;
+            }
+            
             //$PRKA,8E,240913,083130,S,im*0A
+            
             position = new Position();
             extendedInfo = new ExtendedInfoFormatter(getProtocol());
             
             position.setDeviceId(deviceId);
             position.setTableName(tableName);
             position.setImei(deviceImei);
+            position.setCourse(0.0);
+            position.setSpeed(0.0);
+            position.setValid(false);
+            position.setLatitude(0.0);
+            position.setLongitude(0.0);
+                        
+            Integer index = 1;
+            String m = parser.group(index++);
             
+            // Date/Time
+            String dd = parser.group(index++);
+            String mm = parser.group(index++);
+            String yy = parser.group(index++);
             
+            // Time
+            Calendar time = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+            time.clear();
+            time.set(Calendar.YEAR, 2000 + Integer.valueOf("".equals(yy) ? "00" : yy));
+            time.set(Calendar.MONTH, Integer.valueOf("".equals(mm) ? "1" : mm) - 1);
+            time.set(Calendar.DAY_OF_MONTH, Integer.valueOf("".equals(dd) ? "1" : dd));
+            time.set(Calendar.HOUR_OF_DAY, Integer.valueOf(parser.group(index++)));
+            time.set(Calendar.MINUTE, Integer.valueOf(parser.group(index++)));
+            time.set(Calendar.SECOND, Integer.valueOf(parser.group(index++)));
+            position.setTime(time.getTime());
+            //Событие
+            extendedInfo.set("events", parser.group(index++));
+            //Состояние
+            extendedInfo.set("state", parser.group(index++));
             
             return null;
         }
         else if (sentence.startsWith("$PRKB")) {
+            //Pattern patternPRKB = Pattern.compile("\\$PRKB,(\\d{1}),([a-zA-Z_0-9\\+\\=\\/]{0,})");
+            
             //$PRKB,1,=EjMAkAAJAAAPiAzdABEAAAABK94AAAAUAAAArgDP//4=*3D
+            Matcher parser = patternPRKB.matcher(sentence);
+            if (!parser.matches()) {
+                return null;
+            }
+            
             
         }
         //Конец
         else if (sentence.startsWith("$PRKZ")) {
-            //$PRKZ,S*6C
+           
+            Matcher parser = patternPRKZ.matcher(sentence);
+            if (!parser.matches()) {
+                return null;
+            }
+            String str = parser.group(1);
+            
+            String[] data = str.split(",");
+            
+            int index=1;
+            if(data.length > 0){//Слово статуса позиции. 
+                extendedInfo.set("status", parser.group(index++));
+            }
+            if(data.length > 1){//Численный идентификатор геозоны, в которой находится объект.
+                extendedInfo.set("zone", parser.group(index++));
+            }
+            if(data.length > 2){//Горизонтальная скорость относительно поверхности земли (м/сек). 
+                //extendedInfo.set("speed", parser.group(index++));
+                position.setSpeed(Double.parseDouble(parser.group(index++)) * 3.6);
+            }
+            if(data.length > 3){//Путевой угол (угл. градусы) 
+                position.setCourse(Double.parseDouble(parser.group(index++)));
+            }
+            if(data.length > 4){//Значение идентификатора (например, RFID-метки) активного в данный момент аксессуара. 
+                extendedInfo.set("rfid", parser.group(index++));
+            }
             
         }
         //Номер logId точки в устройстве
@@ -138,25 +229,22 @@ public class RoadKeyProtocolDecoder extends BaseProtocolDecoder {
                 return null;
             }
 
-            // Create new position
-            //Position position = new Position();
-            //ExtendedInfoFormatter extendedInfo = new ExtendedInfoFormatter(getProtocol());
-            //position.setDeviceId(deviceId);
-            //position.setTableName(tableName);
-            //position.setImei(deviceImei);
-
             Integer index = 1;
 
             // Time
-            Calendar time = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-            time.set(Calendar.HOUR_OF_DAY, Integer.valueOf(parser.group(index++)));
-            time.set(Calendar.MINUTE, Integer.valueOf(parser.group(index++)));
-            time.set(Calendar.SECOND, Integer.valueOf(parser.group(index++)));
-            time.set(Calendar.MILLISECOND, 0);
-            position.setTime(time.getTime());
-
+            if(position.getTime() == null){
+                Calendar time = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+                time.set(Calendar.HOUR_OF_DAY, Integer.valueOf(parser.group(index++)));
+                time.set(Calendar.MINUTE, Integer.valueOf(parser.group(index++)));
+                time.set(Calendar.SECOND, Integer.valueOf(parser.group(index++)));
+                time.set(Calendar.MILLISECOND, 0);
+                position.setTime(time.getTime());
+            }
+            else{
+                index+=3;
+            }
             // Validity
-            position.setValid(true);
+            //position.setValid(true);
 
             // Latitude
             Double latitude = Double.valueOf(parser.group(index++));
@@ -171,19 +259,41 @@ public class RoadKeyProtocolDecoder extends BaseProtocolDecoder {
             position.setLongitude(longitude);
             
             // Speed
-            position.setSpeed(0.0);
+            //position.setSpeed(0.0);
             
             // Course
-            position.setCourse(0.0);
-
+            //position.setCourse(0.0);
+            
+           
+            //"(\\d{0,}),"+//тип решение
+            index++;                    
+            //количество используемых спутников
+            int satellites = Integer.valueOf(parser.group(index++));
+            extendedInfo.set("satellites", satellites);   
+            position.setValid(satellites > 3);
+            
+            //геометрический фактор, HDOP
+            extendedInfo.set("hdop", parser.group(index++));
             // Altitude
-            position.setAltitude(0.0);
-
-            //position.setExtendedInfo(extendedInfo.getStyle(getDataManager().getStyleInfo()));
-            //return position;
+            position.setAltitude(Double.parseDouble(parser.group(index++)));
         }
         
         return null;
     }
 
+    private byte[] parseData(String data){
+        if(data.charAt(0) == '='){
+            byte[] decoded = Base64.getDecoder().decode(data.substring(1));
+            return decoded;
+        }
+        else if(data.charAt(0) == '+' || data.charAt(0) == '-'){
+            //Integer val = Integer.parseInt(data);
+            //return val
+        }
+        else {
+            return ChannelBufferTools.convertHexString(data);
+        }
+        return null;
+    }
+    
 }
--- test/org/traccar/protocol/RoadKeyProtocolDecoderTest.java
+++ test/org/traccar/protocol/RoadKeyProtocolDecoderTest.java
@@ -1,5 +1,7 @@
 package org.traccar.protocol;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.traccar.helper.TestDataManager;
 import static org.traccar.helper.DecoderVerifier.verify;
 import static org.junit.Assert.assertNull;
@@ -9,66 +11,18 @@ public class RoadKeyProtocolDecoderTest {
 
     @Test
     public void testDecode() throws Exception {
-
         RoadKeyProtocolDecoder decoder = new RoadKeyProtocolDecoder(new TestDataManager(), null, null);
         
         assertNull(decoder.decode(null, null, "!NM311-06080801023 Nadiya 1.9c.4012 (c) Road Key"));        
         assertNull(decoder.decode(null, null, "\u0002353976014438992\u0003"));
         
+        //assertNull(decoder.decode(null, null, "$PRKA,8E,,083130,,im*0A"));
         assertNull(decoder.decode(null, null, "$PRKA,8E,240913,083130,S,im*0A"));
         assertNull(decoder.decode(null, null, "$PRKB,1,=EjMAkAAJAAAPiAzdABEAAAABK94AAAAUAAAArgDP//4=*3D"));
         assertNull(decoder.decode(null, null, "$GPGGA,083130,4849.266,N,03301.323,E,1,5,3.6,135.3,M*2C"));
         assertNull(decoder.decode(null, null, "$PRKZ,S*6C"));
         verify(decoder.decode(null, null,"\u0002233615\u0003"));
         
-        /*
-        assertNull(decoder.decode(null, null, "$GPFID,ID123456ABC"));
-
-        assertNull(decoder.decode(null, null, "$PGID,359853000144328*0F"));
-
-        assertNull(decoder.decode(null, null, "$PCPTI,CradlePoint Test,184453,184453.0,6F*57"));
-        
-        assertNull(decoder.decode(null, null, "IMEI 351467108700000"));
-
-        verify(decoder.decode(null, null,
-                "$GPRMC,094907.000,A,6000.5332,N,03020.5192,E,1.17,60.26,091111,,*33"));
-
-        verify(decoder.decode(null, null,
-                "$GPRMC,115528.000,A,6000.5432,N,03020.4948,E,,,091111,,*06"));
-        
-        verify(decoder.decode(null, null,
-                "$GPRMC,064411.000,A,3717.240078,N,00603.046984,W,0.000,1,010313,,,A*6C"));
-        
-        verify(decoder.decode(null, null,
-                "$GPGGA,000000.0,4337.200755,N,11611.955704,W,1,05,3.5,825.5,M,-11.0,M,,*6F"));
-        
-        verify(decoder.decode(null, null,
-                "$GPGGA,000000,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"));
-        
-        verify(decoder.decode(null, null,
-                "$GPRMA,V,0000.00,S,00000.00,E,,,00.0,000.,11.,E*7"));
-        
-        verify(decoder.decode(null, null,
-                "$TRCCR,20140101001122.333,V,60.0,-100.0,1.1,2.2,3.3,4.4,*00"));
-        
-        verify(decoder.decode(null, null,
-                "$TRCCR,20140111000000.000,A,60.000000,60.000000,0.00,0.00,0.00,50,*3a"));
-        
-        verify(decoder.decode(null, null,
-                "$GPRMC,125735.000,A,6010.34349,N,02445.72838,E,1.0,101.7,050509,6.9,W,A*1F"));
-
-        verify(decoder.decode(null, null,
-                "$GPGGA,000000.000,6010.34349,N,02445.72838,E,1,05,1.7,0.9,M,35.1,M,,*59"));
-        
-        verify(decoder.decode(null, null,
-                "123456789$GPGGA,000000.000,4610.1676,N,00606.4586,E,0,00,4.3,0.0,M,50.7,M,,0000*59"));
-        
-        verify(decoder.decode(null, null,
-                "123456789$GPRMC,155708.252,V,4610.1676,N,00606.4586,E,000.0,000.0,060214,,,N*76"));
-        
-        verify(decoder.decode(null, null,
-                "990000561287964,$GPRMC,213516.0,A,4337.216791,N,11611.995877,W,0.0,335.4,181214,,,A * 72"));
-        */
     }
 
 }
